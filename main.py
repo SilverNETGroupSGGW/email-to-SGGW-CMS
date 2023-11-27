2c90e3945e24901f4ddfba03f314626fc4ff1294
ThePhaseless <kukubaorch@gmail.com>
2023-11-27 11:42:25 +0100

    Refactor processAttachmentsForNLastestEmail function to processAttachmentsForLastestValidEmail

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from io import BytesIO
 import lib.email_client as email_client
 import lib.api_client as api_client
+from models.emailMessage import EmailMessage
 from models.schedule import Schedule
 from lib.logger import logger
 
 
-def processAttachmentsForNLastestEmail(i: int):
+def processAttachmentsForLastestValidEmail():
     # Get allowed senders from env
     import os
     allowedSenders = (os.environ.get("ALLOWED_SENDERS") or "").split(",")
@@ -15,25 +16,30 @@ def processAttachmentsForNLastestEmail(i: int):
     if email_prefix != "":
         email_prefix += "+"
 
-    # Get latest email
-    latestEmail = email_client.getNLatestEmail(0) # 0 = latest email, 1 = second latest email, etc.
+    i:int = -1
+    latestEmail = EmailMessage()
+    while i < 10:
+        i += 1
+        # Get latest email
+        latestEmail = email_client.getNLatestEmail(i) # 0 = latest email, 1 = second latest email, etc.
+        if latestEmail == None:
+            logger.info("No emails found, skipping...")
+            return
+        # Check if email send to email with prefix
+        if not latestEmail.recipient.startswith(email_prefix):
+            print("Email not sent to email with prefix, skipping...")
+            continue
 
-    if latestEmail == None:
-        return
+        # Check if sender is allowed
+        if latestEmail.sender.split("<")[1].strip(">") not in allowedSenders:
+            logger.info("Sender " + latestEmail.sender + " is not allowed, skipping...")
+            continue
 
-    # Check if email send to email with prefix
-    if not latestEmail.recipient.startswith(email_prefix):
-        print("Email not sent to email with prefix, skipping...")
-        return
+        if len(latestEmail.attachments) == 0:
+            logger.info("No attachments, skipping...")
+            continue
+        break
 
-    # Check if sender is allowed
-    if latestEmail.sender.split("<")[1].strip(">") not in allowedSenders:
-        logger.info("Sender " + latestEmail.sender + " is not allowed, skipping...")
-        return
-
-    if len(latestEmail.attachments) == 0:
-        logger.info("No attachments, skipping...")
-        return
     for attachment in latestEmail.attachments:
         if attachment.name.endswith(".xlsx"):
             logger.info("Found Excel file, processing...")
@@ -51,5 +57,5 @@ def processAttachmentsForNLastestEmail(i: int):
         else:
             logger.warning("Unknown file type, skipping...")
 
-processAttachmentsForNLastestEmail(0)
-email_client.listenForEmails(processAttachmentsForNLastestEmail)
+processAttachmentsForLastestValidEmail()
+email_client.listenForEmails(processAttachmentsForLastestValidEmail)
